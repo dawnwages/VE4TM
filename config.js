9994f1ce0273307bb4a32b389b9c1980efa493e5
Dawn Wages <dawn.wages@gmail.com>
2020-10-28 16:00:27 -0400

    Update config.js

--- config.js
+++ config.js
@@ -9,29 +9,29 @@ module.exports = {
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-eventually/`, // This path is subpath of your hosting https://domain/portfolio
   // social
-  heading: 'Eventually',
+  heading: 'Volunteer Engineers 4 The Movement',
   subHeading:
-    "A simple template for telling the world when you'll launch your next big thing.",
+    "Black Tech Professionals interested in using their skills for the advancement of Black people.",
   socialLinks: [
     {
       icon: 'fa-github',
       name: 'Github',
-      url: 'https://github.com/anubhavsrivastava',
+      url: 'https://github.com/VE4TM',
     },
     {
       icon: 'fa-twitter',
       name: 'Twitter',
-      url: 'https://twitter.com/onlyanubhav',
+      url: 'https://twitter.com/VE4TM',
     },
     {
       icon: 'fa-facebook',
       name: 'Facebook',
-      url: 'https://facebook.com/theanubhav',
+      url: 'https://facebook.com/VE4TM',
     },
     {
       icon: 'fa-envelope-o',
       name: 'Email',
-      url: 'mailto:anubhav.srivastava00@gmail.com',
+      url: 'mailto:dawn.wages@gmail.com',
     },
   ],
 };
